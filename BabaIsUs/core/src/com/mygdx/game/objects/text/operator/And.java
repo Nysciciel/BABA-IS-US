36aa44934f51c5751e865c8507c038cdc03026f0
Nysciciel <maron.vincent@outlook.fr>
2019-06-18 09:23:21 +0200

    Post-Merge corrections

--- BabaIsUs/core/src/com/mygdx/game/objects/text/operator/And.java
+++ BabaIsUs/core/src/com/mygdx/game/objects/text/operator/And.java
@@ -1,13 +1,12 @@
 package com.mygdx.game.objects.text.operator;
 
-import com.mygdx.game.Level;
+import com.mygdx.game.*;
 import com.mygdx.game.objects.text.Operator;
 
 public class And extends Operator {
 
-	public And(Level lvl, int x, int y, int orientation) {
-		super(lvl, x, y, orientation);
+	public And(Location loc, int x, int y, int orientation) {
+		super(loc, x, y, orientation);
 
 	}
-
 }
