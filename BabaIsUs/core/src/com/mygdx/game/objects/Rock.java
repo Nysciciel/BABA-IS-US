4fc4d68bc145a5113d804e97942c630300c94cee
Gabriel Hoché <gabriel.hoche@telecom-paristech.fr>
2019-06-19 17:51:32 +0200

    Ajout des textures des Rocks

--- BabaIsUs/core/src/com/mygdx/game/objects/Rock.java
+++ BabaIsUs/core/src/com/mygdx/game/objects/Rock.java
@@ -3,11 +3,18 @@ package com.mygdx.game.objects;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.*;
 import com.mygdx.game.utils.Constants;
 
 public class Rock extends Item {
+	private TextureAtlas textureAtlas;
+	private Animation animation;
+	private float elapsedTime = 0;
+
 	public Rock(Location loc,
 			int x, int y, int orientation) {
 		super(loc, x, y, orientation);
@@ -26,10 +33,19 @@ public class Rock extends Item {
 	}
 
 	public void render(SpriteBatch sb){
+		textureAtlas = new TextureAtlas(Gdx.files.internal("RockSheet.txt"));
+		animation = new Animation(1/3f, textureAtlas.getRegions());
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		TextureRegion test = (TextureRegion) animation.getKeyFrame(elapsedTime, true);
 		int h_ratio = Constants.WINDOW_HEIGHT/(loc.getLevelHeigh());
 		int w_ratio = Constants.WINDOW_WIDTH/(loc.getLevelWidth());
 		int size = Math.min(h_ratio,w_ratio);
-		sb.draw(texture,x*size,y*size,size,size);
+		sb.draw(test,x*size,y*size,size,size);
+	}
+
+	public void dispose(){
+		textureAtlas.dispose();
+		texture.dispose();
 	}
 
 }
