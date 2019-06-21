7dd12b0ede12175f9f2172afa7e73fa40fe39efe
Gabriel Hoché <gabriel.hoche@telecom-paristech.fr>
2019-06-21 18:42:37 +0200

    ajout de Texture de BabaText

--- BabaIsUs/core/src/com/mygdx/game/objects/text/item_ref/BabaText.java
+++ BabaIsUs/core/src/com/mygdx/game/objects/text/item_ref/BabaText.java
@@ -10,4 +10,11 @@ public class BabaText extends ItemRef {
 		// TODO Auto-generated constructor stub
 	}
 
+	public String[] getSpriteUsed(){
+		String[] spriteUsed = new String[2];
+		spriteUsed[0]="BabaText0";
+		spriteUsed[1]="BabaText1";
+		return(spriteUsed);
+	}
+
 }
