91600c3d2d3ce4ebdc6e895edcf8aae6eebcce16
Maxime Bouthors <maxbouthors@gmail.com>
2019-06-25 20:29:33 +0200

    Has texture updated

---
+++ BabaIsUs/core/src/com/mygdx/game/objects/text/item_ref/FlagText.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.objects.text.item_ref;
+
+import com.mygdx.game.Location;
+import com.mygdx.game.objects.text.ItemRef;
+
+public class FlagText extends ItemRef {
+
+	public FlagText(Location loc, int orientation) {
+		super(loc, orientation);
+		// TODO Auto-generated constructor stub
+	}
+
+}
