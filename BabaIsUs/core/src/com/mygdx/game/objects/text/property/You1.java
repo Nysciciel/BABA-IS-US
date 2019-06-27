04b4358846bc2f5b9f714a451e2407c036a2c30c
Charles Pombet <char.pomb@gmail.com>
2019-06-27 10:35:59 +0200

    ajout you1

---
+++ BabaIsUs/core/src/com/mygdx/game/objects/text/property/You1.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.objects.text.property;
+
+import com.mygdx.game.Location;
+import com.mygdx.game.objects.text.Property;
+
+public class You1 extends Property{
+	public You1(Location loc, int orientation) {
+		super(loc, orientation);
+		// TODO Auto-generated constructor stub
+	}
+}
